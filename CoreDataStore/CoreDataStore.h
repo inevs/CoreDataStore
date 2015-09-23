dc467a3721487b202da404a4aec4278cd9d38b32
Sven GuÃànther <sven.guenther@it-agile.de>
2015-09-23 09:08:04 +0200

    Initial Commit

---
+++ CoreDataStore/CoreDataStore.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataStore.h
+//  CoreDataStore
+//
+//  Created by Sven Günther on 23.09.15.
+//  Copyright © 2015 it-agile GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataStore.
+FOUNDATION_EXPORT double CoreDataStoreVersionNumber;
+
+//! Project version string for CoreDataStore.
+FOUNDATION_EXPORT const unsigned char CoreDataStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataStore/PublicHeader.h>
+
+
