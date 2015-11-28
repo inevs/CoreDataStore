acf9332729f5aae982251fe286d5ec5aa70b66ec
Sven Günther <sven.guenther@it-agile.de>
2015-11-28 09:55:18 +0100

    added osx framework

---
+++ CoreDataStoreOSX/CoreDataStoreOSX.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataStoreOSX.h
+//  CoreDataStoreOSX
+//
+//  Created by Sven Günther on 28.11.15.
+//  Copyright © 2015 it-agile GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreDataStoreOSX.
+FOUNDATION_EXPORT double CoreDataStoreOSXVersionNumber;
+
+//! Project version string for CoreDataStoreOSX.
+FOUNDATION_EXPORT const unsigned char CoreDataStoreOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataStoreOSX/PublicHeader.h>
+
+
